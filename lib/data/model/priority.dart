1d07f8d60fa4519b53df3ac0fa6fafef406999e9
Cuong Pham <minhcuong2910@gmail.com>
2019-03-06 22:31:44 +0700

    Update project structure

---
+++ lib/data/model/priority.dart
@@ -0,0 +1,7 @@
+class Priority {
+  final String dbPriority = "priority";
+}
+
+enum Status { PRIORITY_1, PRIORITY_2, PRIORITY_3, PRIORITY_4 }
+
+var priorityText = ["Priority 1", "Priority 2", "Priority 3", "Priority 4"];
